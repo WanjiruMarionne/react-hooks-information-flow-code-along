90db2dff7a89cf36f6b112949b9a636ecef43427
Wanjiru Mburu <wanjirumarionne@gmail.com>
2024-02-03 23:05:42 +0300

    Completed React Information Flow

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,46 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
+/*function Child({ onChangeColor }) {
   return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+}*/
+
+/*function Child({ onChangeColor }) {
+  console.log(onChangeColor);
+  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+
+  return (
+    <div
+      onClick={onChangeColor}
+      className="child"
+      style={{ backgroundColor: "#FFF" }}
+    />
+  );
+}*/
+
+/*function Child({ onChangeColor, color }) {
+  return (
+    <div
+      onClick={onChangeColor}
+      className="child"
+      style={{ backgroundColor: color }}
+    />
+  );
+}*/
+
+function Child({ onChangeColor, color }) {
+  function handleClick() {
+    const newColor = getRandomColor();
+    onChangeColor(newColor);
+  }
+
+  return (
+    <div
+      onClick={handleClick}
+      className="child"
+      style={{ backgroundColor: color }}
+    />
+  );
 }
 
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 import Child from "./Child";
 
-function Parent() {
+/*function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
 
@@ -10,6 +10,44 @@ function Parent() {
     <div className="parent" style={{ backgroundColor: color }}>
       <Child />
       <Child />
+    </div>
+  );
+}*/
+
+function Parent() {
+  const randomColor = getRandomColor();
+  const [color, setColor] = useState(randomColor); // initial value for color state
+  const [childrenColor, setChildrenColor] = useState("#FFF");
+
+  /*function handleChangeColor() {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor); // update color state to a new value
+  }*/
+
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);
+    setChildrenColor(newChildColor);
+  }
+
+  /*return (
+    <div className="parent" style={{ backgroundColor: color }}>
+      <Child />
+      <Child />
+    </div>
+  );*/
+
+  /*return (
+    <div className="parent" style={{ backgroundColor: color }}>
+      <Child onChangeColor={handleChangeColor} />
+      <Child onChangeColor={handleChangeColor} />
+    </div>
+  );*/
+
+  return (
+    <div className="parent" style={{ backgroundColor: color }}>
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
     </div>
   );
 }
